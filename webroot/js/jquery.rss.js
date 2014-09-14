ec28263cd50b3ecd84635492b9e525b9c44a2e8f
Martin Moscosa <mmoscosa@gmail.com>
2014-09-14 02:18:27 -0500

    rss https ?

--- webroot/js/jquery.rss.js
+++ webroot/js/jquery.rss.js
@@ -28,7 +28,7 @@
   RSS.htmlTags = ["doctype", "html", "head", "title", "base", "link", "meta", "style", "script", "noscript", "body", "article", "nav", "aside", "section", "header", "footer", "h1-h6", "hgroup", "address", "p", "hr", "pre", "blockquote", "ol", "ul", "li", "dl", "dt", "dd", "figure", "figcaption", "div", "table", "caption", "thead", "tbody", "tfoot", "tr", "th", "td", "col", "colgroup", "form", "fieldset", "legend", "label", "input", "button", "select", "datalist", "optgroup", "option", "textarea", "keygen", "output", "progress", "meter", "details", "summary", "command", "menu", "del", "ins", "img", "iframe", "embed", "object", "param", "video", "audio", "source", "canvas", "track", "map", "area", "a", "em", "strong", "i", "b", "u", "s", "small", "abbr", "q", "cite", "dfn", "sub", "sup", "time", "code", "kbd", "samp", "var", "mark", "bdi", "bdo", "ruby", "rt", "rp", "span", "br", "wbr"]
 
   RSS.prototype.load = function(callback) {
-    var apiProtocol = "http" + (this.options.ssl ? "s" : "")
+    var apiProtocol = "https" + (this.options.ssl ? "s" : "")
       , apiHost     = apiProtocol + "://ajax.googleapis.com/ajax/services/feed/load"
       , apiUrl      = apiHost + "?v=1.0&output=" + this.options.outputMode + "&callback=?&q=" + encodeURIComponent(this.url)
 
